35eea3481ee0fb2413e66bb2cb7693ab4f455dfe
joyfive <joy_five@kakao.com>
2022-10-06 10:50:38 +0900

    update html&css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <form className='input-box'>
-        <input type={text} className='input-field' onChange={onChange}/>
-        <button type={submit} className='input-btn' onClick={onSubmit}> 추가하기 </button>
+        <input type='text' className='input-field' onChange={onChange} />
+        <button type='submit' className='input-btn' onClick={onSubmit} > 추가하기 </button>
       </form>
-      <h1>Todo List</h1>
       <div className='list-wrap'>
+        <h1>Todo List</h1>
+        <hr></hr>
         <ul className='todo-list'>
           <li className='todo-item'>react를 배워봅시다.</li>
           <li className='todo-item'>useState를 배워봅시다.</li>
